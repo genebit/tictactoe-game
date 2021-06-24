a940870a34cec9ccdefe44ee8aaadd4a4ef80f00
genebit <genebitara@gmail.com>
2021-06-24 13:06:38 +0800

    Added test.js Added grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  var grid = 3;
+  for (var x = 0; x < width; x += width/grid) {
+		for (var y = 0; y < height; y += height/grid) {
+			stroke(0);
+			strokeWeight(4);
+			line(x, 0, x, height);
+			line(0, y, width, y);
+		}
+	}
 }
---
+++ test.js
@@ -0,0 +1,7 @@
+
+
+let arr = [
+    [ null, null, null ],
+    [ null, null, null ],
+    [ null, null, null ]
+];
